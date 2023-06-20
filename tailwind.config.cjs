7f03ce44dd5b47214cbe28e859c6929c3a261842
lukasbelka <lukasbelka@gmail.com>
2023-06-20 14:53:44 +0200

    template adjusted

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+		theme: {
+		  screens: {
+			sm: '480px',
+			md: '768px',
+			lg: '976px',
+			xl: '1440px',
+		  },
+		  colors: {
+			'dark': '#1a1a1a',
+			'smoke': '#f2f2f2',
+			'blue': '#1fb6ff',
+			'purple': '#7e5bef',
+			'pink': '#ff49db',
+			'orange': '#ff7849',
+			'green': '#13ce66',
+			'yellow': '#ffc82c',
+			'gray-dark': '#273444',
+			'gray': '#8492a6',
+			'gray-light': '#d3dce6',
+		  },
+		  fontFamily: {
+			sans: ['Graphik', 'sans-serif'],
+			serif: ['Merriweather', 'serif'],
+		  },
+		  extend: {
+			spacing: {
+			  '128': '32rem',
+			  '144': '36rem',
+			},
+			borderRadius: {
+			  '4xl': '2rem',
+			}
+		  }
+		}
+	  }
