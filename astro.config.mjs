081e1b943e37b8487d46f3f8d4022615550497d6
lukasbelka <lukasbelka@gmail.com>
2023-06-27 21:52:56 +0200

    Netlify adapter added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,27 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from "@astrojs/tailwind";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
     syntaxHighlight: 'shiki',
     shikiConfig: {
-    theme: 'dark-plus',
-    // Enable word wrap to prevent horizontal scrolling
-    wrap: true,
-  }},
+      theme: 'dark-plus',
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true
+    }
+  },
   site: 'https://example.com',
-  integrations: [mdx(),
-    sitemap(),
-    tailwind({
-      // Disable injecting a basic `base.css` import on every page.
-      config: { applyBaseStyles: false },
-    })],
-},
-  );
+  integrations: [mdx(), sitemap(), tailwind({
+    // Disable injecting a basic `base.css` import on every page.
+    config: {
+      applyBaseStyles: false
+    }
+  })],
+  output: "server",
+  adapter: netlify()
+});
